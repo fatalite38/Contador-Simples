54b2fd2522beeced3cdab97cbe8c569efc8d2cf5
Luiz Fernando <luizfernandodp1103@gmail.com>
2022-05-20 12:41:37 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,32 @@
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    
+    
+}
+
+addEventListener("click", function() {
+    if (currentNumber <= -1) {
+        document.getElementById("currentNumber").style.color = 'red';
+    
+    }else if (currentNumber == 0){
+        document.getElementById("currentNumber").style.color = 'black';
+    }else{
+        document.getElementById("currentNumber").style.color = 'blue';
+    }
+})
+
+/*addEventListener("click", function() {
+    if (currentNumber == 0) {
+    document.getElementById("currentNumber").style.color = 'black';
+    }
+})*/
